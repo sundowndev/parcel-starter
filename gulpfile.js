1ff39cf8c64f2ac2cf071a6a7cb497cb9c1f0598
NeOMakinG-PC\NeOMakinG <chewbak1708@hotmail.fr>
2017-11-04 12:51:53 +0100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var input = './public/css/scss/**/*.scss';
+var output = './public/css';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+// ... variables
+var autoprefixerOptions = {
+  browsers: [ 'last 2 version',
+      '> 1%',
+      'ie >= 9',
+      'ie_mob >= 10',
+      'ff >= 30',
+      'chrome >= 34',
+      'safari >= 7',
+      'opera >= 23',
+      'ios >= 7',
+      'android >= 4',
+      'bb >= 10']
+};
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
